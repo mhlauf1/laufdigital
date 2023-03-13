82986be987c8c34cb973ea7a65a9aa00357acffb
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro.local>
2023-03-12 21:45:52 -0500

    working on projects scrolling sidebar functionality, going to start using framer motion

--- utils/data/index.js
+++ utils/data/index.js
@@ -1,3 +1,5 @@
 export { accompishmentsData } from './accompishmentsData';
 export { statisticsData } from './statisticsData';
 export { projectData } from './projectData';
+export { servicesData } from './servicesData';
+
